326b864f6cc8c9e28a68cac7b9a28ecab48d1d44
Abdiel Reynaldo Cuevas Javier <cuevasjavier52@gmail.com>
2026-05-21 19:28:17 -0400

    Initialize variables and print their values
    Added variable initializations and print statements for debugging.

--- ejercicio46.java
+++ ejercicio46.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
 
 public class ejercicio46 {
+
     public static void main(String[] args) {
         //1. Declarar los tipos de datos
         int a;
+        a = 1;
+
         int x, y;
+        x = 2;
+        y = 3;
         double x1;
+        x1 = 2.5;
         double x2, y2;
-        int a1, b1, c1 = 5;
+        x2 = 3.5;
+        y2 = 4.5;
+        int a1 = 5,
+            b1 = 5,
+            c1 = 5;
         boolean sw = false;
         String cad = null;
         final double PI = 3.14;
+
+        System.err.println(a);
+        System.err.println(x);
+        System.err.println(y);
+        System.err.println(x1);
+        System.err.println(x2);
+        System.err.println(y2);
+        System.err.println(a1);
+        System.err.println(b1);
+        System.err.println(c1);
+        System.err.println(sw);
+        System.err.println(cad);
+        System.err.println(PI);
 
         //2. Convertir cadena "200" a un entero en la variable E
         String cadena = "200";
@@ -38,13 +61,11 @@ public class ejercicio46 {
         String bb = aa;
         System.out.println("Ejercicio 6: " + bb);
 
-
         // Ejemplo 2:
         Scanner sc = new Scanner(System.in);
 
         // Ejemplo 3:
         String nombre = "Ana";
         System.out.println("Ejercicio 6: " + nombre);
-
     }
 }
