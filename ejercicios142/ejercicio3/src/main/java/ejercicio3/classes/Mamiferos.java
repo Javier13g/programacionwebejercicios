30dd57d2d6c2e66fe9f50e97c472568b5f433e19
Abdiel Reynaldo Cuevas Javier <cuevasjavier52@gmail.com>
2026-06-01 14:45:17 +0000

    Ejercicio 3

---
+++ ejercicios142/ejercicio3/src/main/java/ejercicio3/classes/Mamiferos.java
@@ -0,0 +1,34 @@
+package ejercicio3.classes;
+
+public abstract class Mamiferos {
+    
+    private String nombre;
+    private String raza;
+    private String tipoAnimal;
+    private String fechaNacimiento;
+    private float peso;
+
+    public Mamiferos(String nombre, String raza, String tipoAnimal, String fechaNacimiento, float peso) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.tipoAnimal = tipoAnimal;
+        this.fechaNacimiento = fechaNacimiento;
+        this.peso = peso;
+    }
+
+    public void comer() {
+        System.out.println(this.nombre + " está comiendo.");
+    }
+
+    public void tipoAnimal() {
+        System.out.println("El tipo de animal es: " + this.tipoAnimal);
+    }
+
+    public abstract void comunicarse();
+
+    public String getNombre() { return nombre; }
+    public String getRaza() { return raza; }
+    public String getTipoAnimal() { return tipoAnimal; }
+    public String getFechaNacimiento() { return fechaNacimiento; }
+    public float getPeso() { return peso; }
+}
---
+++ ejercicios142/ejercicio3/src/main/java/ejercicio3/classes/Perro.java
@@ -0,0 +1,20 @@
+package ejercicio3.classes;
+
+public class Perro extends Mamiferos {
+    
+    private String lugarEntrenamiento;
+
+    public Perro(String nombre, String raza, String tipoAnimal, String fechaNacimiento, float peso, String lugarEntrenamiento) {
+        super(nombre, raza, tipoAnimal, fechaNacimiento, peso);
+        this.lugarEntrenamiento = lugarEntrenamiento;
+    }
+
+    public String getLugarEntrenamiento() {
+        return lugarEntrenamiento;
+    }
+
+    @Override
+    public void comunicarse() {
+        System.out.println("guau guau");
+    }
+}
